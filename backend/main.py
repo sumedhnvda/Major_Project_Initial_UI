888f79c9db01a404aad21653ec41757552864705
sumedhnvda <sumedhnavuda@outlook.com>
2025-12-02 09:56:08 +0530

    final added

--- backend/main.py
+++ backend/main.py
@@ -40,7 +40,8 @@ app = FastAPI(lifespan=lifespan)
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://saraswati.sumedhnavuda.com/"
+    "https://saraswati.sumedhnavuda.com/",
+    "https://saraswati.sumedhnavuda.com"
 ]
 
 app.add_middleware(
